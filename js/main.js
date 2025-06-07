a9458aeb7a8a808d723e0a1ccec430331a7883d9
unknown <nandodivinodev@gmail.com>
2025-06-07 16:27:04 -0300

    alteração das imagens usadas como respostas, implemento de efeitos com css nas respostas e pequenos ajustes de responsavidade, uso de css nas animações com criação e remoção de classes na execução do programa

--- js/main.js
+++ js/main.js
@@ -12,7 +12,11 @@ form.addEventListener('submit', (e) => {  //funcao executada quando o formulario
 
     if (validatriangulo(ladoA, ladoB, ladoC)) { //chamando a funcao somlados() e jogando os valores dos lados para a funcao, a mesma verifica a soma dos lados para obter a resposta e retornar se é possivel ou não criar o triângulo.
         condicao(ladoA, ladoB, ladoC)//chamando a funcao condica() e jogando os valores dos lados para a funcao pois não é possivel acessar diretamente as constantes do lado de fora do escopo que estão.
-        document.querySelector('#resp').classList.add('respostas')
+        const respBox = document.querySelector('#resp');
+        respBox.classList.add('respostas', 'show-flip');
+
+        // Remove o efeito após a animação para permitir novas animações
+        setTimeout(() => respBox.classList.remove('show-flip'), 800);
     }
     else {
         resp1.innerHTML = 'ERRO: Não é possível formar um triângulo!';
@@ -25,28 +29,28 @@ form.addEventListener('submit', (e) => {  //funcao executada quando o formulario
 
 function condicao(valor1, valor2, valor3) { //funcao que verifica as condicoes, retornando um resultado caso a condicao seja válida
     if (valor1 === valor2 && valor2 === valor3) {
-        resp1.innerHTML = '<img src="img/trianguloequilatero.png">'
+        resp1.innerHTML = '<img src="img/equilatero.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO EQUILÁTERO'
     }
     if (valor1 === valor2 && valor2 != valor3) {
-        resp1.innerHTML = '<img src="img/trianguloisosceles.png">'
+        resp1.innerHTML = '<img src="img/isosceles.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ISÓSCELES'
     }
     if (valor2 === valor3 && valor3 != valor1) {
-        resp1.innerHTML = '<img src="img/trianguloisosceles.png">'
+        resp1.innerHTML = '<img src="img/isosceles.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ISÓSCELES'
     }
     if (valor1 === valor3 && valor1 != valor2) {
-        resp1.innerHTML = '<img src="img/trianguloisosceles.png">'
+        resp1.innerHTML = '<img src="img/isosceles.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ISÓSCELES'
     }
     if (valor1 != valor2 && valor2 != valor3 && valor3 != valor1) {
-        resp1.innerHTML = '<img src="img/trianguloescaleno.png">'
+        resp1.innerHTML = '<img src="img/escaleno.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ESCALENO'
     }
 }
 
 
-function validatriangulo(a, b, c) {
+function validatriangulo(a, b, c) {/*parte lógica do código*/
     return a + b > c && a + c > b && b + c > a;
 }
