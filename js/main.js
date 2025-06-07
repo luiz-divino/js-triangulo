a602b72357d7a1d1e38322b5fe3c7420fc486a3a
unknown <nandodivinodev@gmail.com>
2025-06-06 22:47:20 -0300

    config de responsavidade para diferentes aparelhos com css, resolução da lógica do programa e adição das imagens de acordo com a resposta com js, além da implementação de textos informativos com html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 let form = document.querySelector('form');  //selecionando o formulario
 let resp1 = document.querySelector('h2');   //selecionando o h3 que posteriormente receberá um valor e irá exibir a resposta
 let resp2 = document.querySelector('h3');   //selecionando 0 h2 que posteriormente receberá um valor e irá exibir a resposta
@@ -11,30 +12,36 @@ form.addEventListener('submit', (e) => {  //funcao executada quando o formulario
 
     if (validatriangulo(ladoA, ladoB, ladoC)) { //chamando a funcao somlados() e jogando os valores dos lados para a funcao, a mesma verifica a soma dos lados para obter a resposta e retornar se é possivel ou não criar o triângulo.
         condicao(ladoA, ladoB, ladoC)//chamando a funcao condica() e jogando os valores dos lados para a funcao pois não é possivel acessar diretamente as constantes do lado de fora do escopo que estão.
-    } else {
-        resp1.innerText = 'ERRO!!!'
+        document.querySelector('#resp').classList.add('respostas')
     }
-})
+    else {
+        resp1.innerHTML = 'ERRO: Não é possível formar um triângulo!';
+        resp2.innerText = 'Lembre-se: A soma de dois lados deve ser sempre maior que o terceiro lado.';
+    }
+}
+)
+
+
 
 function condicao(valor1, valor2, valor3) { //funcao que verifica as condicoes, retornando um resultado caso a condicao seja válida
     if (valor1 === valor2 && valor2 === valor3) {
-        resp1.innerText = 'LADOS PODEM FORMAR UM TRIÂNGULO'
+        resp1.innerHTML = '<img src="img/trianguloequilatero.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO EQUILÁTERO'
     }
     if (valor1 === valor2 && valor2 != valor3) {
-        resp1.innerText = 'LADOS PODEM FORMAR UM TRIÂNGULO'
+        resp1.innerHTML = '<img src="img/trianguloisosceles.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ISÓSCELES'
     }
     if (valor2 === valor3 && valor3 != valor1) {
-        resp1.innerText = 'LADOS PODEM FORMAR UM TRIÂNGULO'
+        resp1.innerHTML = '<img src="img/trianguloisosceles.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ISÓSCELES'
     }
     if (valor1 === valor3 && valor1 != valor2) {
-        resp1.innerText = 'LADOS PODEM FORMAR UM TRIÂNGULO'
+        resp1.innerHTML = '<img src="img/trianguloisosceles.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ISÓSCELES'
     }
     if (valor1 != valor2 && valor2 != valor3 && valor3 != valor1) {
-        resp1.innerText = 'LADOS PODEM FORMAR UM TRIÂNGULO'
+        resp1.innerHTML = '<img src="img/trianguloescaleno.png">'
         resp2.innerText = 'TIPO: TRIÂNGULO ESCALENO'
     }
 }
